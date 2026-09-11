d7c5265ad6d87d0538d661684358a3c3ba9346ca
wakazaragnarom-stack <wakazaragnarom@gmail.com>
2026-09-11 08:56:36 -0500

    refactor: use unified application entrypoint

--- main.tsx
+++ main.tsx
@@ -1,10 +1,13 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
-import App from './App.tsx';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import UnifiedApp from './UnifiedApp.tsx';
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(
+const root = document.getElementById('root');
+if (!root) throw new Error('No se encontró el elemento root');
+
+createRoot(root).render(
   <StrictMode>
-    <App />
+    <UnifiedApp />
   </StrictMode>,
 );
